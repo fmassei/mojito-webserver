ef5ae3e0bd7dafec82409e4e989a0a2c1f4cf738
fmassei <fmassei@227c6c96-933b-11de-a13a-71b910e68cc8>
2009-10-02 14:14:48 +0000

    Statistic modules tries

--- branches/working-0.2/modules/mod_stat/mod_stat.c
+++ branches/working-0.2/modules/mod_stat/mod_stat.c
@@ -17,14 +17,95 @@
     along with Mojito.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "../modules.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "../modules.h"
+#include "../../response.h"
 
+typedef unsigned long stat_type_t;
+static struct stat_counters_s {
+    stat_type_t init,
+                fini,
+                accept,
+                can_run,
+                presend,
+                prehead,
+                send,
+                postsend;
+} counters;
+
+static char *stat_page = "<html><body>init: %10d<br />accept: %10d<br /></body></html>";
+
+static void send_statistics(int sock, struct request_s *req)
+{
+    int clen;
+    char *buf;
+    clen = strlen(stat_page)+15;
+    if ((buf = malloc(clen))==NULL) {
+        head_push_code(HRESP_500);
+        return;
+    }
+    sprintf(buf, stat_page, counters.init, counters.accept);
+    header_push_code(HRESP_200);
+    header_push_contentlength(clen);
+    header_push_contenttype("text/html");
+    header_send(sock);
+    if (req->method==M_HEAD)
+        return;
+    write(sock, buf, clen);
+}
+
+static int _init(void)
+{
+    counters.init =
+        counters.fini =
+        counters.accept =
+        counters.can_run =
+        counters.presend =
+        counters.prehead =
+        counters.send =
+        counters.postsend = 0;
+    ++counters.init;
+    return MOD_OK;
+}
+static int _fini(void)
+{
+    ++counters.fini;
+    return MOD_OK;
+}
 static int _on_accept(void)
 {
-    DEBUG_LOG((LOG_DEBUG, "Accepted."));
-    return 0;
+    ++counters.accept;
+    return MOD_OK;
+}
+static int _can_run(void)
+{
+    ++counters.can_run;
+    return MOD_OK;
+}
+static int _on_presend(int sock, struct request_s *req)
+{
+    ++counters.presend;
+    if (!strcmp(req.uri, "statistics")) {
+        send_statistics(sock, req);
+        return MOD_ALLDONE;
+    }
+    return MOD_OK;
+}
+static int _on_prehead(struct stat *st)
+{
+    ++counters.prehead;
+    return MOD_OK;
+}
+static int _on_send(void *v, int i, struct stat *st)
+{
+    ++counters.send;
+    return MOD_OK;
+}
+static int _on_postsend(struct request_s *r, char *c, void *v, struct stat *s)
+{
+    ++counters.postsend;
+    return MOD_OK;
 }
 
 #ifdef MODULE_STATIC
@@ -38,13 +119,13 @@ struct module_s *getmodule()
         return NULL;
     p->name = "stat";
     p->set_params = NULL;
-    p->init = NULL;
-    p->fini = NULL;
+    p->init = _init;
+    p->fini = _fini;
     p->on_accept = _on_accept;
-    p->on_presend = NULL;
-    p->on_prehead = NULL;
-    p->on_send = NULL;
-    p->on_postsend = NULL;
+    p->on_presend = _on_presend;
+    p->on_prehead = _on_prehead;
+    p->on_send = _on_send;
+    p->on_postsend = _on_postsend;
     p->next = p->prev = NULL;
     p->will_run = 1;
     p->category = MODCAT_UNSPEC;
