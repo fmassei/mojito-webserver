071803a3c61a6818537552aba630bda7eca6b9e6
fmassei <fmassei@227c6c96-933b-11de-a13a-71b910e68cc8>
2010-06-10 12:53:09 +0000

    - more on request flow

--- request.h
+++ request.h
@@ -6,16 +6,32 @@
 #include <mmp/mmp_memory.h>
 #include <mmp/mmp_trace.h>
 
+typedef enum request_parse_status_e {
+        REQUEST_PARSE_STATUS_HEAD,
+        REQUEST_PARSE_STATUS_CR1,
+        REQUEST_PARSE_STATUS_LF1,
+        REQUEST_PARSE_STATUS_CR2,
+        REQUEST_PARSE_STATUS_BODY,
+} t_request_parse_status_e;
+
+typedef enum request_method_e {
+        REQUEST_METHOD_GET,
+        REQUEST_METHOD_HEAD,
+        REQUEST_METHOD_POST
+} t_request_method_e;
+
 /* variables needed for request parsing */
 typedef struct request_parse_s {
     int reline;
     int rb, mr, len, pos;
     char c, *buf, *cur_head;
+    t_request_parse_status_e status;
 } t_request_parse_s;
 
 /* the main request object */
 typedef struct request_s {
-    t_request_parse_s   parse;
+    t_request_parse_s parse;
+    t_request_method_e method;
 } t_request_s;
 
 t_request_s *request_create(void);
--- request_parse.c
+++ request_parse.c
@@ -2,6 +2,17 @@
 
 #define SOCKBUFSIZE 8196    /* TODO: calculate this one! */
 
+static int parse_first_line(t_request_s *req, char *line) {return 0;}
+static int parse_header_line(t_request_s *req, char *line) {return 0;}
+/* parse an option */
+static int parse_option(t_request_s *req, char *line)
+{
+    if (req->method==0)
+        return parse_first_line(req, line);
+    else
+        return parse_header_line(req, line);
+}
+
 t_request_parse_e request_parse_read(socket_t *sock, t_request_s *req)
 {
     req->parse.len = req->parse.pos = 0;
@@ -30,7 +41,50 @@ t_request_parse_e request_parse_read(socket_t *sock, t_request_s *req)
         if (req->parse.rb==0) {
             return REQUEST_PARSE_CLOSECONN;
         }
-        return REQUEST_PARSE_FINISH;
+        /* BOA-like loop. An insane mess, but pretty clear after two or three
+         * hours spent on reading this twenty lines. Good luck. */
+        while(req->parse.rb-->0) {
+            req->parse.c = *(req->parse.buf+req->parse.pos);
+            if (req->parse.c=='\0')
+                continue;
+            switch(req->parse.status) {
+            case REQUEST_PARSE_STATUS_HEAD:
+                if (req->parse.c=='\r')
+                    req->parse.status = REQUEST_PARSE_STATUS_CR1;
+                else if (req->parse.c=='\n')
+                    req->parse.status = REQUEST_PARSE_STATUS_LF1;
+                break;
+            case REQUEST_PARSE_STATUS_CR1:
+                if (req->parse.c=='\n')
+                    req->parse.status = REQUEST_PARSE_STATUS_LF1;
+                else if (req->parse.c=='\r')
+                    req->parse.status = REQUEST_PARSE_STATUS_HEAD;
+                break;
+            case REQUEST_PARSE_STATUS_LF1:
+                if (req->parse.c=='\r')
+                    req->parse.status = REQUEST_PARSE_STATUS_CR2;
+                else if (req->parse.c=='\n')
+                    req->parse.status = REQUEST_PARSE_STATUS_BODY;
+                else
+                    req->parse.status = REQUEST_PARSE_STATUS_HEAD;
+                break;
+            case REQUEST_PARSE_STATUS_CR2:
+                if (req->parse.c=='\n')
+                    req->parse.status = REQUEST_PARSE_STATUS_BODY;
+                else if (req->parse.c=='\r')
+                    req->parse.status = REQUEST_PARSE_STATUS_HEAD;
+                break;
+            }
+            ++req->parse.pos;
+            if (req->parse.status==REQUEST_PARSE_STATUS_LF1) {
+                /* header found */
+                req->parse.buf[req->parse.pos-2] = '\0';
+                parse_option(req, req->parse.cur_head);
+                req->parse.cur_head = req->parse.buf+req->parse.pos;
+            } else if (req->parse.status==REQUEST_PARSE_STATUS_BODY) {
+            }
+        }
+        /* return REQUEST_PARSE_FINISH; somewhere */ 
     }
     return REQUEST_PARSE_ERROR; /* this should never happen */
 }
