c46a69e811804657ba2bfffba0926682fccade97
fmassei <fmassei@227c6c96-933b-11de-a13a-71b910e68cc8>
2010-09-23 22:23:15 +0000

    - linux project fixes - mmp lib from static to dynamic

--- branches/working-0.4/module_loader.c
+++ branches/working-0.4/module_loader.c
@@ -19,11 +19,28 @@
 #include "module_loader.h"
 
 #ifndef DISABLE_DYNAMIC
+static char *get_module_filename(const char *name)
+{
+#ifdef _WIN32
+    static const char *ext = ".dll";
+    static const char *pre = "";
+#else
+    static const char *ext = ".so.1";
+    static const char *pre = "lib";
+#endif
+    char *ret;
+    if ((ret = xmalloc(strlen(pre)+strlen(name)+strlen(ext)+1))==NULL)
+        return NULL;
+    sprintf(ret, "%s%s%s", pre, name, ext);
+    return ret;
+}
+
 ret_t module_loader_load(const t_config_s *params)
 {
     t_mmp_listelem_s *p;
     t_module_s *mod;
     t_config_module_s *mod_conf;
+    char *mod_filename;
     if (params==NULL || params->modules==NULL) {
         mmp_setError(MMP_ERR_PARAMS);
         return MMP_ERR_PARAMS;
@@ -34,7 +51,12 @@ ret_t module_loader_load(const t_config_s *params)
             continue;
         mmp_trace_reset();
         printf("Loading module %s... ", mod_conf->name);
-        mod = module_add_dynamic(mod_conf->name);
+        if ((mod_filename = get_module_filename(mod_conf->name))==NULL) {
+            printf("ENOMEM assigning name!?!\n");
+            continue;
+        }
+        mod = module_add_dynamic(mod_filename);
+        xfree(mod_filename);
         if (mod==NULL) {
             printf("ERR\n");
             mmp_trace_print(stdout);
--- branches/working-0.4/prj/linux/run_mojito.sh
+++ branches/working-0.4/prj/linux/run_mojito.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-LD_LIBRARY_PATH="../../deps/lib/linux" ./mojito
+LD_LIBRARY_PATH=".:../../deps/lib/linux" ./mojito
